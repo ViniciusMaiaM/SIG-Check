1f1cd60e53dcb4c0e60673d60c868f0f4feb9b52
Vinicius <viniciusmaiamarinho1@gmail.com>
2022-09-05 20:36:01 -0300

    alterando arquivo principal

--- check.c
+++ check.c
@@ -1,13 +1,43 @@
 #include <stdio.h>
-#include <unistd.h> 
 #include <stdlib.h>
+#include <stdlib.h>
+#include "user.h"
 
-char telaprinc(void);
-char tela_check(void);
-void sobre(void);
+/////
+// Assinatura das funções
+char tela_principal(void);
+void tela_sobre(void);
 
-char telaprinc(void){
-  char esc;
+
+/////
+// Programa principal
+int main(void) {
+
+  char modulo;
+  while (modulo != '0'){
+    modulo = tela_principal();
+    switch (modulo)
+    {
+    case '1':
+      tela_usuario();
+      break;
+    
+    case '4':
+      tela_sobre();
+      break;
+
+    default:
+      break;
+    }
+  }
+  return 0;
+}
+
+
+/////
+// Funções
+
+char tela_principal(void) {
   system("clear||cls");
   printf("\n");
   printf(" ____________________________________________________ \n");
@@ -21,17 +51,20 @@ char telaprinc(void){
   printf("|                                                    |\n");
   printf("|             - - - - SIG-Check - - - -              |\n");
   printf("|                                                    |\n");
-  printf("|             1 - Modulo Sobre                       |\n");
-  printf("|             2 - Check                              |\n");
+  printf("|             1 - Modulo Usuario                     |\n");
+  printf("|             2 - Modulo Gerencia                    |\n");
+  printf("|             3 - Cadastrar cheque                   |\n");
+  printf("|             4 - Sobre o sistema                    |\n");
   printf("|             0 - Sair                               |\n");
   printf("|                                                    |\n");
   printf("|____________________________________________________|\n");
-  printf("Escolha a opcao desejada: ");
-  scanf("%c", &esc);
-  return esc;
+  printf("\n");
+  printf("Pressione enter para continuar!\n");
+  getchar();
 }
 
-void sobre(void){
+
+void tela_sobre(void) {
   system("clear||cls");
   printf(" ____________________________________________________ \n");
   printf("|                                                    |\n");
@@ -54,139 +87,6 @@ void sobre(void){
   printf("|           - Thamiris Borges(Thami03)               |\n");
   printf("|           - Vinicius Maia(ViniciusMaiaM)           |\n");
   printf("|____________________________________________________|\n");
-}
-
-char tela_check(void){
-  char esc;
-  system("clear||cls");
-  printf(" ____________________________________________________ \n");
-  printf("|                                                    |\n");
-  printf("|    Universidade Federal do Rio Grande do Norte     |\n");
-  printf("|            Centro de Ensino Superior               |\n");
-  printf("|     Departamento de Computacao e Tecnologia        |\n");
-  printf("|         Disciplina DCT1106 -- Programacao          |\n");
-  printf("|                Projeto SIG-Check                   |\n");
-  printf("|____________________________________________________|\n");
-  printf("|                                                    |\n");
-  printf("|          - - - - Modulo Check - - - -              |\n");
-  printf("|                                                    |\n");
-  printf("|             1 - Cadastrar cheque                   |\n");
-  printf("|             2 - Relatorio de cheques recebidos     |\n");
-  printf("|             0 - Voltar                             |\n");
-  printf("|                                                    |\n");
-  printf("|____________________________________________________|\n");
-  printf("Escolha a opcao desejada: ");
-  scanf("%c", &esc);
-  return esc;
-}
-
-char check(void){
-  char esc;
-  system("clear||cls");
-  printf(" ____________________________________________________ \n");
-  printf("|                                                    |\n");
-  printf("|    Universidade Federal do Rio Grande do Norte     |\n");
-  printf("|            Centro de Ensino Superior               |\n");
-  printf("|     Departamento de Computacao e Tecnologia        |\n");
-  printf("|         Disciplina DCT1106 -- Programacao          |\n");
-  printf("|                Projeto SIG-Check                   |\n");
-  printf("|____________________________________________________|\n");
-  printf("|                                                    |\n");
-  printf("|          - - - - Modulo Check - - - -              |\n");
-  printf("|                                                    |\n");
-  printf("|             1 - Cadastrar cheque                   |\n");
-  printf("|             2 - Relatorio de cheques recebidos     |\n");
-  printf("|             0 - Voltar                             |\n");
-  printf("|                                                    |\n");
-  printf("|____________________________________________________|\n");
-  printf("Escolha a opcao desejada: ");
-  scanf("%c", &esc);
-  return esc;
-}
-char cadastrar_check(void){
-  char esc;
-  system("clear||cls");
-  printf(" ____________________________________________________ \n");
-  printf("|                                                    |\n");
-  printf("|    Universidade Federal do Rio Grande do Norte     |\n");
-  printf("|            Centro de Ensino Superior               |\n");
-  printf("|     Departamento de Computacao e Tecnologia        |\n");
-  printf("|         Disciplina DCT1106 -- Programacao          |\n");
-  printf("|                Projeto SIG-Check                   |\n");
-  printf("|____________________________________________________|\n");
-  printf("|                                                    |\n");
-  printf("|          - - - - Cadastro de  Cheque - - - -       |\n");
-  printf("|                                                    |\n");
-  printf("|             1 - A Vista                            |\n");
-  printf("|             2 - A Prazo                            |\n");
-  printf("|             0 - Voltar                             |\n");
-  printf("|                                                    |\n");
-  printf("|____________________________________________________|\n");
-  printf("Escolha a opcao desejada: ");
-  scanf("%c", &esc);
-  return esc;
-}
-char relatorio_check(void){
-  char esc;
-  system("clear||cls");
-  printf(" ____________________________________________________ \n");
-  printf("|                                                    |\n");
-  printf("|    Universidade Federal do Rio Grande do Norte     |\n");
-  printf("|            Centro de Ensino Superior               |\n");
-  printf("|     Departamento de Computacao e Tecnologia        |\n");
-  printf("|         Disciplina DCT1106 -- Programacao          |\n");
-  printf("|                Projeto SIG-Check                   |\n");
-  printf("|____________________________________________________|\n");
-  printf("|                                                    |\n");
-  printf("|          - - - - Relatorio de Cheque - - - -       |\n");
-  printf("|                                                    |\n");
-  printf("|             0 - Voltar                             |\n");
-  printf("|                                                    |\n");
-  printf("|____________________________________________________|\n");
-  printf("Escolha a opcao desejada: ");
-  scanf("%c", &esc);
-  return esc;
-}
-int main()
-{ 
-  char modulo; 
-  char mod_check;
-
-
-  modulo = telaprinc();
-  switch (modulo){
-  case '1':
-    sobre();
-    break;
-    
-  case '2':
-	check();
-	break;
-	
-  case '0':
-    printf("Fim do programa!\n");
-    break;
-  
-  default:
-    printf("Por favor insira uma opcao valida.\n");
-  }
-  
-  mod_check = tela_check();
-  switch (mod_check){
-	case '1':
-    cadastrar_check();
-    break;
-    
-  case '2':
-	relatorio_check();
-	break;
-	
-  case '0':
-    telaprinc();
-    break;
-
-    default:
-	  printf("Por favor insira uma opcao valida.\n");
- }
-  
+  printf("Pressione enter para continuar!\n");
+  getchar();
 }
