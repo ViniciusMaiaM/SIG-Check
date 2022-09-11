18155345be1278450dbea1c0e62ec10e20131922
Vinicius <viniciusmaiamarinho1@gmail.com>
2022-09-11 10:46:13 -0300

    Vinicius: add funções no header

--- movimentacao.c
+++ movimentacao.c
@@ -50,6 +50,7 @@ void tela_gerencia(void) {
 
 void tela_pesquisar_cheque(void) {
     system("clear||cls");
+    int num_cheque;
     printf("\n");
     printf(" ____________________________________________________ \n");
     printf("|                                                    |\n");
@@ -62,7 +63,8 @@ void tela_pesquisar_cheque(void) {
     printf("|                                                    |\n");
     printf("|          - - - - Pesquisar cheque - - - -          |\n");
     printf("|                                                    |\n");
-    printf("|   Informe o Numero do cheque (apenas numeros):     |\n");
+    printf("|   Informe o Numero do cheque (apenas numeros): ");
+    scanf("%d",&num_cheque);
     printf("|                                                    |\n");
     printf("|                                                    |\n");
     printf("|                                                    |\n");
@@ -75,6 +77,7 @@ void tela_pesquisar_cheque(void) {
 
 void tela_alterar_cheque(void) {
     system("clear||cls");
+    int num_cheque;
     printf("\n");
     printf(" ____________________________________________________ \n");
     printf("|                                                    |\n");
@@ -87,7 +90,8 @@ void tela_alterar_cheque(void) {
     printf("|                                                    |\n");
     printf("|          - - - - Alterar cheque - - - -            |\n");
     printf("|                                                    |\n");
-    printf("|   Informe o Numero do cheque (apenas numeros):     |\n");
+    printf("|   Informe o Numero do cheque (apenas numeros): ");
+    scanf("%d",&num_cheque);
     printf("|                                                    |\n");
     printf("|                                                    |\n");
     printf("|                                                    |\n");
@@ -100,6 +104,7 @@ void tela_alterar_cheque(void) {
 
 void tela_excluir_cheque(void) {
     system("clear||cls");
+    int num_cheque;
     printf("\n");
     printf(" ____________________________________________________ \n");
     printf("|                                                    |\n");
@@ -112,7 +117,8 @@ void tela_excluir_cheque(void) {
     printf("|                                                    |\n");
     printf("|          - - - - Excluir cheque - - - -            |\n");
     printf("|                                                    |\n");
-    printf("|   Informe o Numero do cheque (apenas numeros):     |\n");
+    printf("|   Informe o Numero do cheque (apenas numeros): ");
+    scanf("%d",&num_cheque);
     printf("|                                                    |\n");
     printf("|                                                    |\n");
     printf("|                                                    |\n");
@@ -138,63 +144,6 @@ void tela_lucros(void) {
     printf("|         - - - - Caixa lucros - - - -               |\n");
     printf("|                                                    |\n");
     printf("|              Saldo:                                |\n");
-    printf("|                                                    |\n");
-    printf("|____________________________________________________|\n");
-    printf("\n");
-    printf("Pressione enter para continuar!\n");
-    getchar();
-}
-
-
-void tela_pagar_cheque(void) {
-    system("clear||cls");
-    printf("\n");
-    printf(" ____________________________________________________ \n");
-    printf("|                                                    |\n");
-    printf("|    Universidade Federal do Rio Grande do Norte     |\n");
-    printf("|            Centro de Ensino Superior               |\n");
-    printf("|     Departamento de Computacao e Tecnologia        |\n");
-    printf("|         Disciplina DCT1106 -- Programacao          |\n");
-    printf("|                Projeto SIG-Check                   |\n");
-    printf("|____________________________________________________|\n");
-    printf("|                                                    |\n");
-    printf("|          - - - - Pagar cheque - - - -              |\n");
-    printf("|                                                    |\n");
-    printf("|          ID do usuario:                            |\n");
-    printf("|          Codigo do banco:                          |\n");
-    printf("|          Numero do cheque:                         |\n");
-    printf("|          Valor do cheque:                          |\n");
-    printf("|          Data do pagamento:                        |\n");
-    printf("|          Valor a pagar:                            |\n");
-    printf("|                                                    |\n");
-    printf("|____________________________________________________|\n");
-    printf("\n");
-    printf("Pressione enter para continuar!\n");
-    getchar();
-}
-
-void cadastrar_cheque(void) {
-    system("clear||cls");
-    printf("\n");
-    printf(" ____________________________________________________ \n");
-    printf("|                                                    |\n");
-    printf("|    Universidade Federal do Rio Grande do Norte     |\n");
-    printf("|            Centro de Ensino Superior               |\n");
-    printf("|     Departamento de Computacao e Tecnologia        |\n");
-    printf("|         Disciplina DCT1106 -- Programacao          |\n");
-    printf("|                Projeto SIG-Check                   |\n");
-    printf("|____________________________________________________|\n");
-    printf("|                                                    |\n");
-    printf("|          - - - - Cadastrar cheque  - - - -         |\n");
-    printf("|                                                    |\n");
-    printf("|          Tipo (A vista || A prazo):                |\n");
-    printf("|          Numero do cheque:                         |\n");
-    printf("|          Codigo do banco:                          |\n");
-    printf("|          ID do usuario:                            |\n");
-    printf("|          Data do vendimento(dd/mm/aaaa):           |\n");
-    printf("|          Valor:                                    |\n");
-    printf("|          Quantidade de parcelas                    |\n");
-    printf("|                                                    |\n");
     printf("|                                                    |\n");
     printf("|____________________________________________________|\n");
     printf("\n");
