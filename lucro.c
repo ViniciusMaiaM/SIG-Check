45f989a2f59721c50130ad37fdbda6ec29d6e03f
Vinicius <viniciusmaiamarinho1@gmail.com>
2022-09-28 07:08:12 -0300

    vinicius: add acentos no módulo lucro

--- lucro.c
+++ lucro.c
@@ -5,25 +5,18 @@
 void relatorio_lucro(void) { 
     system("clear||cls");
     printf("\n");
-    printf(" ____________________________________________________ \n");
-    printf("                                                    \n");
-    printf("    Universidade Federal do Rio Grande do Norte     \n");
-    printf("            Centro de Ensino Superior               \n");
-    printf("     Departamento de Computacao e Tecnologia        \n");
-    printf("         Disciplina DCT1106 -- Programacao          \n");
-    printf("                Projeto SIG-Check                   \n");
     printf("____________________________________________________\n");
     printf("                                                    \n");
-    printf("          - - - - Relatorio Lucro  - - - -          \n");
+    printf("          - - - - Relatório Lucro  - - - -          \n");
     printf("             Nome:Thamiris                          \n");
-    printf("             Numero do cheque:032                   \n");
+    printf("             Número do cheque:032                   \n");
     printf("             Nome do banco:Nubank                   \n");
     printf("             Valor do cheque(R$):79.50              \n");
     printf("             Taxa:0,15%%                            \n");
     printf("             Lucro(R$):11.9                         \n");
     printf("____________________________________________________\n");
     printf("             Nome:Vinicius                          \n");
-    printf("             Numero do cheque:021                   \n");
+    printf("             Número do cheque:021                   \n");
     printf("             Nome do banco:Caixa                    \n");
     printf("             Valor do cheque(R$):76.0               \n");
     printf("             Taxa:0,15%%                            \n");
