4d3aeb9e96541b0ccdf8afde2858083f4bf5e48d
Vinicius <viniciusmaiamarinho1@gmail.com>
2022-10-11 04:12:33 -0300

    vinicius: add modularização em pastas de movimentacao

---
+++ movimentacao/movimentacao.h
@@ -0,0 +1,6 @@
+void tela_movimenta(void);
+void tela_pesquisar_cheque(void);
+void tela_alterar_cheque(void);
+void tela_gerencia(void);
+void escolha_tela(char escolha);
+void relatorio_movimento(void);
