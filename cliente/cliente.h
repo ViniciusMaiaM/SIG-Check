33e3806d9d4aec6ecddddccd9de23cb935b381fc
Vinicius <viniciusmaiamarinho1@gmail.com>
2022-10-17 20:31:49 -0300

    vinicius: removendo função de validação de cpf e correção de chamadas

--- cliente/cliente.h
+++ cliente/cliente.h
@@ -1,6 +1,6 @@
 void tela_cliente(void);
 void escolha_cliente(char);
-int cadastro_cliente(void);
+void cadastro_cliente(void);
 void pesquisar_cliente(void);
 void excluir_cliente(void);
 void relatorio_cliente(void);
