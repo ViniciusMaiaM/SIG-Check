3cbcbf05c724cfb8b41e2b5ba5487fd1c977f70a
thami03 <thamiris.b@escolar.ifrn.edu.br>
2022-10-16 22:43:24 -0300

    Validador cpf

--- cliente/cliente.h
+++ cliente/cliente.h
@@ -1,6 +1,6 @@
 void tela_cliente(void);
 void escolha_cliente(char);
-void cadastro_cliente(void);
+int cadastro_cliente(void);
 void pesquisar_cliente(void);
 void excluir_cliente(void);
 void relatorio_cliente(void);
