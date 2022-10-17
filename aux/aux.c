b8ce4c441141466b7c6ff5c30577debcdcda2591
Vinicius <viniciusmaiamarinho1@gmail.com>
2022-10-17 18:40:19 -0300

    vinicius: add validação de data

--- aux/aux.c
+++ aux/aux.c
@@ -3,6 +3,8 @@
 #include "aux.h"
 
 int ddd[27] = {11,21,27,31,41,47,51,61,62,63,65,67,68,69,71,79,81,82,83,84,85,86,91,92,95,96,98};
+int meses[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+
 
 char input(void) //função de input
 {
@@ -51,4 +53,77 @@ int busca_num(int tam, char celular[]){
         }
     }
     return 1;
+}
+
+
+///////////
+//Criado com base no algoritmo do professor Flavius Gorgonio
+//////////
+int data_valida_int(int dia, int mes, int ano){
+    int md;
+    if (ano < 0 || mes > 12 || mes < 0 || dia < 1){
+        return 0;
+    }
+    
+    if  (mes == 2) {
+        if (bissexto(ano)){
+            md = 29;
+        }
+        else{
+            md = 28;
+        }
+    }
+    
+    else if (mes == 4 || mes == 6 || mes == 9 || mes == 11){
+        md = meses[mes-1];
+    }
+
+    else{
+        md = meses[mes-1];
+    }
+
+    if (dia > md){
+        return 0;
+    }
+
+    return 1;
+}
+
+///////////
+//Criado com base no algoritmo do professor Flavius Gorgonio
+//////////
+int bissexto (int ano){ 
+    if ((ano % 4 == 0) && (ano % 100 != 0)){
+        return 1;
+    }
+
+    else if (ano % 400 == 0){
+        return 1;
+    }
+
+    else{
+        return 0;
+    }
+}
+
+int data_valida_str(char data[]){
+    int dia, mes, ano;
+
+    if (strlen(data) != 10){
+        return 0;
+    }
+
+    if (data[2] != '/' || data[5] != '/'){
+        return 0;
+    }
+
+    dia = ((data[0] - '0') * 10) + (data[1] - '0');
+    mes = ((data[3] - '0') * 10) + (data[4] - '0');
+    ano = ((data[6] - '0') * 1000) + ((data[7] - '0') * 100) + ((data[8] - '0') * 10) + (data[9] - '0');
+    
+    if(data_valida_int(dia,mes,ano)){
+        return 1;
+    }
+
+    return 0;
 }
--- aux/aux.h
+++ aux/aux.h
@@ -1,4 +1,7 @@
 char input(void);
-int valida_cel(char celular[]);
 int busca_ddd(int busca);
 int busca_num(int tam, char celular[]);
+int bissexto(int ano);
+int data_valida_int(int dia, int mes, int ano);
+int data_valida_str(char data[]);
+int valida_cel(char celular[]);
