664f787d6785946f4871781700468f1c72f1c974
Vinicius <viniciusmaiamarinho1@gmail.com>
2022-10-26 09:39:24 -0300

    viniciu: add struct de cheque

--- cheque/cheque.c
+++ cheque/cheque.c
@@ -56,13 +56,8 @@ void tela_cheque(void) {
 } 
 
 void cadastrar_cheque(void) { //Tela que recebe informacoes iniciais do user, precisamos criar validacoes
-  char cheque_num[10];
-  char cod_banco[10];
-  char nome_cliente[10];
-  char agencia[10];
-  char num_conta[10];
-  int valor;
-  char data[10];
+  Cheque* che;
+  che = (Cheque*) malloc(sizeof(Cheque));
   system("clear||cls");
   printf("____________________________________________________\n");
   printf("                                                    \n");
@@ -71,45 +66,46 @@ void cadastrar_cheque(void) { //Tela que recebe informacoes iniciais do user, pr
   printf("____________________________________________________\n");
   printf("                                                    \n");
   printf("          Nome do cliente: "); //Assim ele vai pegar todas as informações do usuario do arquivo
-  scanf("%50[^\n]",nome_cliente);
+  scanf("%50[^\n]",che->nome_cliente);
   getchar();
 
   do {
     printf("          Número do cheque: ");
-    scanf("%[0-9]",cheque_num);
+    scanf("%[0-9]",che->cheque_num);
     getchar();
-  } while(!valida_dig(cheque_num));
+  } while(!valida_dig(che->cheque_num));
 
   do {
     printf("          Código do banco: ");
-    scanf("%[0-9]",cod_banco);
+    scanf("%[0-9]",che->cod_banco);
     getchar();
-  } while(!valida_dig(cod_banco));
+  } while(!valida_dig(che->cod_banco));
   
   do {
     printf("          Número agência: ");
-    scanf("%[0-9]",agencia);
+    scanf("%[0-9]",che->agencia);
     getchar();
-  } while(!valida_dig(agencia));
+  } while(!valida_dig(che->agencia));
   
   do {
     printf("          Número conta: ");
-    scanf("%[0-9]",num_conta);
+    scanf("%[0-9]",che->num_conta);
     getchar();
-  } while(!valida_dig(num_conta));
+  } while(!valida_dig(che->num_conta));
   
   printf("          Valor do cheque: ");
-  scanf("%d",&valor);
+  scanf("%d",&che->valor);
   getchar();
 
   do{
     printf("          Data para ser descontado: ");
-    scanf("%[0-9 / -]", data);
+    scanf("%[0-9 / -]", che->data);
     getchar();
-  }while(!data_str(data));
+  }while(!data_str(che->data));
   printf("                                                    \n");
   printf("                                                    \n");
   printf("                                                    \n");
+  print_struct(che);
   printf("____________________________________________________\n");
   printf("\nPressione enter para continuar!\n");
   getchar();
@@ -190,4 +186,5 @@ void relatorio_cheque(){
   printf("\nPressione enter para continuar!\n");
   getchar();
 }
+
 
