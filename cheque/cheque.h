9d591e6048ef5d01067212e9a4f2d352c765f9f5
Vinicius <viniciusmaiamarinho1@gmail.com>
2022-11-10 16:06:35 -0300

    vinicius: add assinatura de funções para arquivo

--- cheque/cheque.h
+++ cheque/cheque.h
@@ -12,14 +12,14 @@ struct cheque {
 };
 
 
-void tela_cheque(void);
-Cheque* cadastrar_cheque(void);
-Cheque* busca_cheque();
+void att_cheque(Cheque* che);
+void exibe_cheque(Cheque* che);
+void excluir_cheque(Cheque* che);
 void grava_cheque(Cheque* che);
+void lista_cheque(void);
 void muda_tela_cheque(char escolha);
 void pesquisar_cheque(void);
-void excluir_cheque(void);
-void relatorio_cheque();
-void exibe_cheque(Cheque* che);
-//int valida_string(char array[]);
-void lista_cheque();
+void relatorio_cheque(void);
+void tela_cheque(void);
+Cheque* busca_cheque(void);
+Cheque* cadastrar_cheque(void);
