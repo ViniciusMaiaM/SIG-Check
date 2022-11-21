ee77735105bc1f2466a4c96ac41aa1215244f261
Thamiris Borges <87441572+Thami03@users.noreply.github.com>
2022-11-21 01:47:01 -0300

    Crud caixa

--- caixa/caixa.h
+++ caixa/caixa.h
@@ -1,2 +1,30 @@
+typedef struct caixa Caixa;
+
+struct caixa {
+    char cpf_cliente[30];
+    char entrada_saida_caixa[2];
+    float valor_caixa;
+    char data_caixa[40];
+    int id_cheque;
+    int id_transacao_caixa;
+    char status;
+};
+
 void tela_caixa(void);
 void escolha_caixa(char);
+void grava_caixa(Caixa* cai);
+void ler_cpf(char* cpf);
+void pesquisar_caixa(void);
+void exibe_caixa(Caixa* cai);
+void excluir_caixa(Caixa* cai);
+void att_caixa(Caixa* cai);
+void lista_caixa(void);
+
+int id_tra();
+
+Caixa* cadastrar_caixa(void);
+Caixa* busca_caixa(void);
+
+
+
+
