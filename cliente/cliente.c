0baf27cc7ee6cf0f75251f7afbb8d7f0c692faa9
Vinicius <viniciusmaiamarinho1@gmail.com>
2022-12-07 05:05:35 -0300

    vinicius: corrigindo atualiazação de cliente

--- cliente/cliente.c
+++ cliente/cliente.c
@@ -258,10 +258,6 @@ void att_cliente(Cliente *cli)
     FILE *fp;
     char resp;
     char escolha;
-    char nome[100];
-    char cpf[30];
-    char cel[30];
-    char email[100];
 
     if ((cli == NULL) || (cli->status == 'x'))
     {
@@ -270,6 +266,7 @@ void att_cliente(Cliente *cli)
     }
 
     fp = fopen("cliente.txt", "r+t");
+
     if (fp == NULL)
     {
         printf("Ocorreu um erro na abertura do arquivo, não é possivel continuar o programa");
@@ -291,7 +288,9 @@ void att_cliente(Cliente *cli)
         printf("             2 - Cpf                                \n");
         printf("             3 - Celular                            \n");
         printf("             4 - Email                              \n");
-        printf("             5 - Tudo                               \n");
+        printf("             5 - Data de nascimento                 \n");
+        printf("             6 - Genero                             \n");
+        printf("             7 - Tudo                               \n");
         printf("             0 - Voltar                             \n");
         printf("                                                    \n");
         printf("____________________________________________________\n");
@@ -304,38 +303,42 @@ void att_cliente(Cliente *cli)
             switch (escolha)
             {
             case '1':
-                ler_nome(nome);
-                strcpy(cli->nome_cliente, nome);
+                ler_nome(cli->nome_cliente);
                 printf("\nCliente editado com sucesso!\n");
                 break;
 
             case '2':
-                ler_cpf(cpf);
-                strcpy(cli->cpf_cliente, cpf);
+                ler_cpf(cli->cpf_cliente);
                 printf("\nCliente editado com sucesso!\n");
                 break;
 
             case '3':
-                ler_cel(cel);
-                strcpy(cli->cel_cliente, cel);
+                ler_cel(cli->cel_cliente);
                 printf("\nCliente editado com sucesso!\n");
                 break;
 
             case '4':
-                ler_email(email);
-                strcpy(cli->email_cliente, email);
+                ler_email(cli->email_cliente);
                 printf("\nCliente editado com sucesso!\n");
                 break;
 
             case '5':
-                ler_nome(nome);
-                strcpy(cli->nome_cliente, nome);
-                ler_cpf(cpf);
-                strcpy(cli->cpf_cliente, cpf);
-                ler_cel(cel);
-                strcpy(cli->cel_cliente, cel);
-                ler_email(email);
-                strcpy(cli->email_cliente, email);
+                ler_nasc(cli->data_nasc);
+                printf("\nCliente editado com sucesso!\n");
+                break;
+
+            case '6':
+                cli->genero = ler_genero(cli->genero);
+                printf("\nCliente editado com sucesso!\n");
+                break;
+
+            case '7':
+                ler_nome(cli->nome_cliente);
+                ler_cpf(cli->cpf_cliente);
+                ler_cel(cli->cel_cliente);
+                ler_email(cli->email_cliente);
+                ler_nasc(cli->data_nasc);
+                cli->genero = ler_genero(cli->genero);
                 printf("\nCliente editado com sucesso!\n");
                 break;
 
@@ -353,6 +356,7 @@ void att_cliente(Cliente *cli)
     {
         printf("\nOk, os dados não foram alterados!\n");
     }
+    
     espera();
     fclose(fp);
 }
